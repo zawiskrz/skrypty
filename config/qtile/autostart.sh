2171d62f9c1469da16328120e1105ec0d32cef43
zawiskrz <112825491+zawiskrz@users.noreply.github.com>
2026-09-01 10:57:04 +0200

    Update autostart.sh

--- config/qtile/autostart.sh
+++ config/qtile/autostart.sh
@@ -1,10 +1,33 @@
 #!/bin/sh
 
-# Ustawienie rozdzielczości ekranu dla GNOME Boxes / KVM (fallback do HDMI-1)
+# --- EKRAM I SPICE (GNOME Boxes / KVM) ---
+# Ustawienie rozdzielczości ekranu (fallback do HDMI-1)
 xrandr --output Virtual-1 --mode 1920x1080 2>/dev/null || xrandr --output HDMI-1 --mode 1920x1080 2>/dev/null &
 
-# Uruchomienie agenta schowka dla GNOME Boxes / SPICE
+# Uruchomienie agenta schowka dla GNOME Boxes / SPICE (kopiowanie/wklejanie z hostem)
 spice-vdagent &
 
-# Uruchomienie kompozytora okien w tle
+# --- WYGLĄD I EFEKTY ---
+# Uruchomienie kompozytora okien w tle (przezroczystość, cienie)
 picom -b &
+
+# --- APLETY DO ZASOBNIKA SYSTEMOWEGO (SYSTRAY) ---
+# Aplet sieci (Wi-Fi / Ethernet)
+nm-tray &
+# Jeśli używasz klasycznego nm-applet, zamień na: nm-applet &
+
+# Aplet sterowania głośnością (PulseAudio / PipeWire)
+pasystray &
+
+# Aplet do obsługi Bluetooth
+blueman-applet &
+
+# Menedżer zasilania (zarządzanie baterią i wygaszaniem ekranu)
+xfce4-power-manager &
+
+# --- POWIADOMIENIA I AUTORYZACJA ---
+# Daemon powiadomień systemowych
+dunst &
+
+# Agent autoryzacji Polkit (wymagany do okien z prośbą o hasło root/sudo)
+/usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 &
